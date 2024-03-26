fd93f12323ddd5e08c6d860994184d038b02f315
LuizFPS04 <luizfpsena04@gmail.com>
2024-03-26 10:19:58 -0300

    create new function

--- aula/src/main/java/aula/git/Apresentacao.java
+++ aula/src/main/java/aula/git/Apresentacao.java
@@ -3,11 +3,17 @@ package aula.git;
 public class Apresentacao {
 
     public void apresentar() {
-        this.lucasSouza();
+        this.luiz();
+        this.apresentese();
     }
 
-    private void lucasSouza() {
+    private void luiz() {
         System.out.println("");
         System.out.println("Estou colaborando com o git");
+    }
+
+    private void apresentese() {
+        System.out.println("");
+        System.out.println("Opa! Tranquilo? Eu me chamo Luiz Felipe, tenho 20 anos e sou um apaixonado por tecnologia!");
     }
 }
